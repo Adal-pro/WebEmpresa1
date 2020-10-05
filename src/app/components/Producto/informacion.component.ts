e00ee3cdf09f064fca8f41d6778bd2d7f8d90e7e
Adalberto Suarez <Adal85@outlook.com>
2020-10-05 18:12:11 -0400

    actualizamos las imagenes

--- src/app/components/Producto/informacion.component.ts
+++ src/app/components/Producto/informacion.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
-
+import {InformacionpersService} from "../../services/informacionpers.service";
 @Component({
   selector: 'app-informacion',
   templateUrl: './informacion.component.html',
 
 })
 export class InformacionComponent  {
+  anio:number = new Date().getFullYear();
+  date:number= new Date().getDate();
+  mes:number = new Date().getMonth()+1;
+  constructor(public _is:InformacionpersService){
+
+  }
+
+
 
 
 }
